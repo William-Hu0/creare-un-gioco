14b81303f5b23c1e8e7407477ff83890ffe8b6d1
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-03-19 09:30:07 +0000

    1

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 function startGame() {
-    myGameArea.start();
-    myGameArea.draw(redSquare);
+  myGameArea.start();
+  myGameArea.draw(redSquare);
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]); 
-    },
-    draw: function(component) {
-        this.context.fillStyle = component.color;
-        this.context.fillRect(component.x, component.y, component.width, component.height);
-          }
-}
+  canvas : document.createElement("canvas"),
+  start : function() {
+      this.canvas.width = 480;
+      this.canvas.height = 270;
+      this.context = this.canvas.getContext("2d");
+      document.body.insertBefore(this.canvas, document.body.childNodes[0]); 
+      this.interval = setInterval(updateGameArea, 20);
+  },
+  draw: function(component) {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.context.fillStyle = component.color;
+      this.context.fillRect(component.x, component.y, component.width, component.height);
+        },
+};
+
 var redSquare = {
-    width: 20,
-    height: 20,
-    x: 10,
-    y: 120,
-    color: "red"
-  };
-  function moveup() {
-    redSquare.y -= 30;
-  }
-  
-  function movedown() {
-    redSquare.y += 30;
-  }
-  
-  function moveleft() {
-    redSquare.x -= 30;
-  }
-  
-  function moveright() {
-    redSquare.x += 30;
-  }
-  function updateGameArea() {
-    myGameArea.draw(redSquare);
-  }
+  width: 20,
+  height: 20,
+  x: 10,
+  y: 120,
+  color: "red"
+};
+function moveup() {
+  redSquare.y -= 30;
+}
+
+function movedown() {
+  redSquare.y += 30;
+}
+
+function moveleft() {
+  redSquare.x -= 30;
+}
+
+function moveright() {
+  redSquare.x += 30;
+}
+function updateGameArea() {
+  myGameArea.draw(redSquare);
+}
